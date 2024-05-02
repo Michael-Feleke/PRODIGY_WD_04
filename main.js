ddd19ac3222c95166e690109b778db542d4bca22
Michael Feleke <felekemichael999@gmail.com>
2024-05-02 09:44:11 +0300

    Main.js to toggle class for humbergure menu

---
+++ main.js
@@ -0,0 +1,7 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector("navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("fa-xmark");
+  navbar.classList.toggle("active");
+};
