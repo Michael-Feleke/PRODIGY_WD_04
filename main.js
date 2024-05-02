9c993d7891428b8b2fd520227f32a95342e5cfaf
Michael Feleke <felekemichael999@gmail.com>
2024-05-02 09:55:01 +0300

    Making sections scrolling

--- main.js
+++ main.js
@@ -5,3 +5,29 @@ menuIcon.onclick = () => {
   menuIcon.classList.toggle("fa-xmark");
   navbar.classList.toggle("active");
 };
+
+let sections = document.querySelectorAll("section");
+let navlinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navlinks.forEach.apply((links) => {
+        links.classList.remove("active");
+        document
+          .queryselector(`header nav a[href*=` + id + `]`)
+          .classList.add("active");
+      });
+    }
+  });
+
+  let header = document.queryselector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  menuIcon.classList.remove("fa-xmark");
+};
